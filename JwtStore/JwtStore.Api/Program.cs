fbf72a77f3f2ea3ee8a3d2e2154f7b8381409110
Lucas Rodrigues <rodrigues14set@hotmail.com>
2023-10-02 13:54:10 -0300

    Ajustando configurações e criando o banco de dados

--- JwtStore/JwtStore.Api/Program.cs
+++ JwtStore/JwtStore.Api/Program.cs
@@ -1,6 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using JwtStore.Api.Extensions;
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddConfiguration();
+builder.AddDatabase();
+builder.AddJwtAuthentication();
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
--- JwtStore/JwtStore.Core/Configuration.cs
+++ JwtStore/JwtStore.Core/Configuration.cs
@@ -2,6 +2,11 @@ namespace JwtStore.Core;
 public static class Configuration
 {
   public static SecretsConfiguration Secrets { get; set; } = new();
+  public static DatabaseConfiguration Database { get; set; } = new();
+  public class DatabaseConfiguration
+  {
+    public string ConnectionString { get; set; } = string.Empty;
+  }
 
   public class SecretsConfiguration
   {
@@ -9,4 +14,5 @@ public static class Configuration
     public string JwtPrivateKey { get; set; } = string.Empty;
     public string PasswordSaltKey { get; set; } = string.Empty;
   }
+
 }
