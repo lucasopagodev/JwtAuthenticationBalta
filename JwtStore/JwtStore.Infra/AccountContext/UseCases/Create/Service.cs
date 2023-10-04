eae62a1ced81a11a9f12d7c42df0734cc41e0641
Lucas Fernandes Lopes Rodrigues <rodrigues14set@hotmail.com>
2023-10-04 15:32:01 -0300

    Iniciando os Repository

---
+++ JwtStore/JwtStore.Infra/AccountContext/UseCases/Create/Service.cs
@@ -0,0 +1,21 @@
+﻿using JwtStore.Core;
+using JwtStore.Core.Contexts.AccountContext.Entities;
+using JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace JwtStore.Infra;
+
+public class Service : IService
+{
+    public async Task SendVerificationEmailAsync(User user, CancellationToken cancellationToken)
+    {
+        var client = new SendGridClient(Configuration.SendGrid.ApiKey);
+        var from = new EmailAddress(Configuration.Email.DefaultFromEmail, Configuration.Email.DefaultFromName);
+        const string subject = "My Wallet - Verifique sua conta";
+        var to = new EmailAddress(user.Email, user.Name);
+        var content = $"{user.Email.Verification.Code}";
+        var message = MailHelper.CreateSingleEmail(from, to, subject, content, content);
+        await client.SendEmailAsync(message, cancellationToken);
+    }
+}
