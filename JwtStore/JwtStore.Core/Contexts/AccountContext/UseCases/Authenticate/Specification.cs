b321986a4e82a9d01639f444dda439ff22634f98
Lucas Rodrigues <rodrigues14set@hotmail.com>
2023-10-10 14:42:44 -0300

    Criando o UseCase de Authenticate

---
+++ JwtStore/JwtStore.Core/Contexts/AccountContext/UseCases/Authenticate/Specification.cs
@@ -0,0 +1,14 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate;
+
+public static class Specification
+{
+  public static Contract<Notification> Ensure(Request request) =>
+    new Contract<Notification>()
+      .Requires()
+      .IsLowerThan(request.Password.Length, 40, "Password", "A senha deve conter menos de 40 caracteres")
+      .IsGreaterThan(request.Password.Length, 8, "Password", "A senha deve conter mais de 8 caracteres")
+      .IsEmail(request.Email, "E-Mail", "E-Mail inválido");
+}
