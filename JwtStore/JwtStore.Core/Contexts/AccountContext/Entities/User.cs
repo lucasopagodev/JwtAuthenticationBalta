e0d1033cca85a590d205728ad1f45565de05cbd9
Lucas Fernandes Lopes Rodrigues <rodrigues14set@hotmail.com>
2023-10-16 12:13:51 -0300

    Criando Roles na aplicação

--- JwtStore/JwtStore.Core/Contexts/AccountContext/Entities/User.cs
+++ JwtStore/JwtStore.Core/Contexts/AccountContext/Entities/User.cs
@@ -25,6 +25,7 @@ public class User : Entity
     public Email Email { get; private set; } = null!;
     public Password Password { get; private set; } = null!;
     public string Image { get; set; } = string.Empty;
+    public IEnumerable<Role> Roles { get; set; } = Enumerable.Empty<Role>();
 
     public void UpdatePassword(string plainTextPassword, string code)
     {
